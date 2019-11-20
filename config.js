4a87ac826dcabb6516f4f429886fea69538bbb72
Alberto García <albertogarv1402@gmail.com>
2019-11-19 20:23:07 -0600

    changed config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/studentsDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/matchdayDB";
 exports.PORT = process.env.PORT || 8080;
