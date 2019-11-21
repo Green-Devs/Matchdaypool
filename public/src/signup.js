0c1a2c9eb4e7e465ec2bda4014e5b4b0b5f52d5c
Alberto García <albertogarv1402@gmail.com>
2019-11-21 17:48:12 -0600

    signup almost finished

--- public/src/signup.js
+++ public/src/signup.js
@@ -11,51 +11,81 @@ function signup() {
         console.log("signup");
 
         if ($("#name").val() && $("#lastname").val() && $("#username").val() && $("#email").val() && $("#password").val() && $("#dob").val()) {
-            let url = "/api/registerUser";
-            let newUser = {
-                name: $("#name").val(), 
-                lastname: $("#lastname").val(), 
-                username: $("#username").val(), 
-                email: $("#email").val(), 
-                password: $("#password").val(),
-                dob: $("#dob").val()
+            
+            if ($("#password").val() == $("#confPassword").val()) {
+                let url = "/api/registerUser";
+                let newUser = {
+                    name: $("#name").val(), 
+                    lastname: $("#lastname").val(), 
+                    username: $("#username").val(), 
+                    email: $("#email").val(), 
+                    password: $("#password").val(),
+                    dob: $("#dob").val()
+                }
+                
+                let settings = {
+                    url: url ,
+                    method: "POST",
+                    data: JSON.stringify(newUser), // info sent to the API
+                    dataType: "JSON", // returned type of the response
+                    contentType: "application/json", // type of sent data in the request
+                    success: function(responseJSON){
+                        console.log("success", responseJSON);
+                        window.location.href = './index.html';
+                    },
+                    error: function(err){
+                        console.log("err", err);
+                    }
+                };
+
+                $.ajax(settings);
+            } else {
+                // show that passwords dont match
             }
             
-            let settings = {
-                url: url ,
-                method: "POST",
-                data: JSON.stringify(newUser), // info sent to the API
-                dataType: "JSON", // returned type of the response
-                contentType: "application/json", // type of sent data in the request
-                success: function(responseJSON){
-                    console.log("success", responseJSON);
-                    window.location.href = './index.html';
-                },
-                error: function(err){
-                    console.log("err", err);
-                }
-            };
-
-            $.ajax(settings);
         } else {
             console.log("falta un valor");
             if (!$("#name").val()) {
                 console.log("no name");
+                document.querySelector("#redName").hidden = false;
+            } else {
+                document.querySelector("#redName").hidden = true;
             }
-            if ($("#lastname").val()) {
 
-            } 
-            if ($("#username").val()) {
+            if (!$("#lastname").val()) {
+                document.querySelector("#redLastname").hidden = false;
+            } else {
+                document.querySelector("#redLastname").hidden = true;
+            }
 
-            } 
-            if ($("#email").val()) {
+            if (!$("#username").val()) {
+                document.querySelector("#redUsername").hidden = false;
+            } else {
+                document.querySelector("#redUsername").hidden = true;
+            }
 
-            } 
-            if ($("#password").val()) {
+            if (!$("#dob").val()) {
+                document.querySelector("#redBirthday").hidden = false;
+            } else {
+                document.querySelector("#redBirthday").hidden = true;
+            }
 
-            } 
-            if ($("#dob").val()) {
-                
+            if (!$("#email").val()) {
+                document.querySelector("#redEmail").hidden = false;
+            } else {
+                document.querySelector("#redEmail").hidden = true;
+            }
+
+            if (!$("#password").val()) {
+                document.querySelector("#redPassword").hidden = false;
+            } else {
+                document.querySelector("#redPassword").hidden = true;
+            }
+
+            if (!$("#confPassword").val()) {
+                document.querySelector("#redConfPassword").hidden = false;
+            } else {
+                document.querySelector("#redConfPassword").hidden = true;
             }
         }
     });
