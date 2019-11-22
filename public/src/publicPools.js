aa3b464be165d43494090c2ca5a0586f2a2ad29b
Alberto García <albertogarv1402@gmail.com>
2019-11-21 18:29:25 -0600

    getting pools changes

--- public/src/publicPools.js
+++ public/src/publicPools.js
@@ -10,32 +10,17 @@ function fetchPublicPools() {
             console.log("success", responseJSON);
 
             for (let i = 0; i < responseJSON.length; i++) {
-                if (!responseJSON.private) {
-                    let id = responseJSON[i].owner;
+            
 
-                    let getSettings = {
-                        url: "/api/getUsers/" + id,
-                        method: "GET",
-                        dataType: "JSON",
-                        success: function (responseJSON) {
-                            owner = responseJSON.name;
-                        },
-                        error: function (err) {
-                            console.log("err", err);
-                        }
-                    }
-
-                    $.ajax(getSettings);
-
-                    $(".listOfPools").append(`
-                    <div class="card mb-3">
-                        <div class="card-body text-white bg-dark">
-                            <h5 class="card-title">${responseJSON[i].name}</h5>
-                            <p class="card-text">${responseJSON[i].desc}</p>
-                            <p class="card-text"><small class="text-muted">Owner: ${owner} Sport: ${responseJSON[i].sport} Cost: ${responseJSON[i].cost}</small></p>
-                        </div>
-                    </div>`);
-                }
+                $(".listOfPools").append(`
+                <div class="card mb-3">
+                    <div class="card-body text-white bg-dark">
+                        <h5 class="card-title">${responseJSON[i].name}</h5>
+                        <p class="card-text">${responseJSON[i].desc}</p>
+                        <p class="card-text"><small class="text-muted">Owner: ${responseJSON[i].ownerName}, ${responseJSON[i].owner} Sport: ${responseJSON[i].sport} Cost: ${responseJSON[i].cost}</small></p>
+                    </div>
+                </div>`);
+                
             }
 
             $(".spinner-border").hide();
